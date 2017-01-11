196266d7a5e68c06071207ca4d2acc9362ce6432
hyegeuncho <hgcho84.kr@gmail.com>
2017-01-12 00:13:36 +0900

    [Haskell] 연습문제 1-2-2-7 풀이중..

--- Language/Haskell/WikibooksHaskell/sourcecode/1-2-2_More_about_lists.hs
+++ Language/Haskell/WikibooksHaskell/sourcecode/1-2-2_More_about_lists.hs
@@ -1,3 +1,5 @@
+-- 1-2-2 More about lists
+
 doubleList :: [Integer] -> [Integer]
 doubleList [] = []
 doubleList (n:ns) = (2 * n) : doubleList ns
@@ -26,3 +28,35 @@ scanSum (n:m:ms) = n : scanSum ((n + m):ms)
 diffs :: [Integer] -> [Integer]
 diffs [a, b] = [b - a]
 diffs (n:m:ms) = (m - n) : diffs (m:ms)
+
+applyToIntegers :: (Integer -> Integer) -> [Integer] -> [Integer]
+applyToIntegers _ [] = []
+applyToIntegers f (n:ns) = (f n) : applyToIntegers f ns
+
+heads :: [[a]] -> [a]
+heads = map head
+
+-- Exercise 1-2-2-6
+-- banjeonList :: [Integer] -> [Integer]
+
+-- Exercise 1-2-2-7
+myLength :: [a] -> Integer
+myLength [] = 0
+myLength (x:xs) = 1 + myLength xs
+
+-- myElementRLEncoder :: [a] -> (Integer, a)
+--myElementRLEncoder (x:xs) = (1 + myLength xs, x)
+myElementRLEncoder :: String -> String
+myElementRLEncoder (x:xs) = concat [show (1 + myLength xs), [x]]
+
+myRLEncoder :: [String] -> [String]
+myRLEncoder = map myElementRLEncoder
+
+myRLEncoderResult :: [String] -> String
+myRLEncoderResult xs = concat (myRLEncoder xs)
+
+myRR :: String -> [String] -> String
+myRR xs = myRLEncoderResult mid
+    where
+    	mid = group xs
+-- myRLEncoderResult (group "aaaabbaaa")
