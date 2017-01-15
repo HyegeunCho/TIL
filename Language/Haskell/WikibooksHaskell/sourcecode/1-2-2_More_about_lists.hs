65ba846a9bdb1780ea6d48d9195843addd2d86b6
hyegeuncho <hgcho84.kr@gmail.com>
2017-01-15 10:05:49 +0900

    [Haskell] 스터디 추가

--- Language/Haskell/WikibooksHaskell/sourcecode/1-2-2_More_about_lists.hs
+++ Language/Haskell/WikibooksHaskell/sourcecode/1-2-2_More_about_lists.hs
@@ -55,8 +55,29 @@ myRLEncoder = map myElementRLEncoder
 myRLEncoderResult :: [String] -> String
 myRLEncoderResult xs = concat (myRLEncoder xs)
 
-myRR :: String -> [String] -> String
-myRR xs = myRLEncoderResult mid
-    where
-    	mid = group xs
+--myRR :: String -> [String] -> String
+--myRR xs = myRLEncoderResult mid
+--    where
+--    	mid = group xs
+
 -- myRLEncoderResult (group "aaaabbaaa")
+
+
+-- head와 tail에 대한 언급
+-- 리스트를 쪼갤 때, head와 tail보다는 (:) 패턴을 사용하는게 낫다.
+-- head와 tail은 빈 리스트에 대해 실패한다.
+-- null을 사용해 빈 리스트를 검사할 수 있으나, 빈 리스트에 대한 (:) 패턴이 더 깔끔하고 깨끗하다.
+
+-- 연습문제 
+-- 1. 이 장의 첫 번쨰 연습문제 모음에 대한 여러분의 해답과 관련하여, scanSum (takeInt 10 [1...])과 
+-- takeInt 10 (scanSum [1...])에 차이가 있는가?
+-- 답 : 결과적으로는 차이가 없다.
+
+-- 2. 리스트에 적용하ㅏ면 마지막 원소와 그 원소를 뺀 나머지를 반환하는 함수를 작성하라. 이 기능은 Prelude가 last와 init 함수로 제공하는 것이다. head와 tail 처럼 빈 리스트를 받으면 폭발한다.
+myLast :: [a] -> a
+myLast [x] = x
+myLast (x:xs) = myLast xs
+
+myInit :: [a] -> [a]
+myInit [x] = []
+myInit (x:xs) = x : myInit xs
