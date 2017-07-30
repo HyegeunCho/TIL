522b40acf7bb33f856a3040649232775b85578f4
HyegeunCho <hgcho84.kr@gmail.com>
2017-07-30 14:04:27 +0900

    [MemoryPost] 리스트단위에서 카드 단위로 출력 - weight 조절 기능 추가

--- angular2/memory-post/src/app/app.component.ts
+++ angular2/memory-post/src/app/app.component.ts
@@ -10,8 +10,12 @@ import { MemoryService } from './memory.service';
 })
 export class AppComponent implements OnInit {
     title = 'Memory post';
-    memories: Memory[];
-    isName: {[id: number] : boolean } = {};
+    currentMemory: Memory;
+    currentIsName: boolean;
+
+    private memories: Memory[];
+    private isName: {[id: number] : boolean } = {};
+    private currentIndex: number;
 
     constructor(private memoryService:MemoryService) {}
 
@@ -22,6 +26,9 @@ export class AppComponent implements OnInit {
             for (var i: number = 0; i < memories.length; i++) {
                 this.isName[memories[i].id] = true;
             }
+            this.currentIndex = 0;
+            this.currentMemory = this.memories[this.currentIndex];
+            this.currentIsName = true;
         }.bind(this));
     }
 
@@ -29,7 +36,28 @@ export class AppComponent implements OnInit {
         this.getMemories();
     }
 
-    onSelect(memory: Memory): void {
-        this.isName[memory.id] = !this.isName[memory.id];
+    onSelect(/*memory: Memory*/): void {
+        //this.isName[memory.id] = !this.isName[memory.id];
+        this.currentIsName = !this.currentIsName;
+    }
+
+    onNext(): void {
+        this.currentIsName = true;
+        this.currentIndex = (this.currentIndex === (this.memories.length - 1) ? this.currentIndex : this.currentIndex + 1);
+        this.currentMemory = this.memories[this.currentIndex];
+    }
+
+    onPrevious(): void {
+        this.currentIsName = true;
+        this.currentIndex = (this.currentIndex === 0 ? 0 : this.currentIndex - 1);
+        this.currentMemory = this.memories[this.currentIndex];
+    }
+
+    onWeightPlus(): void {
+        this.currentMemory.weight++;
+    }
+
+    onWeightMinus(): void {
+        this.currentMemory.weight--;
     }
 }
