59c15af6e84bd56ac8f82ef9105149c6533e1e60
HyegeunCho <hgcho84.kr@gmail.com>
2018-10-08 00:47:56 +0900

    리액트 기술 책 내용 추가

---
+++ Web/react/book_ReactSkill/todolist/src/MyComponent.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+class MyComponent extends Component {
+    static defaultProps = {
+        name: '기본 이름'
+    };
+    render() {
+        return(
+            <div>
+                안녕하세요. 내 이름은 {this.props.name} 입니다.
+            </div>
+        )
+    }
+}
+// MyComponent.defaultProps = {
+//     name: '기본 이름'
+// };
+export default MyComponent;
