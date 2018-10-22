3fc101d30db88a587309d00190f15e41d2717cbf
stzhyegeun <hyegeun.cho@sundaytoz.com>
2018-10-23 00:37:54 +0900

    리액트기술 10.2 추가

---
+++ Web/react/book_ReactSkill/todolist/src/components/App.js
@@ -0,0 +1,16 @@
+import React, {Component} from 'react';
+import PageTemplate from './PageTemplate';
+import TodoInput from './TodoInput';
+
+class App extends Component {
+    
+    render() {
+        return(
+            <PageTemplate>
+                <TodoInput/>
+            </PageTemplate>
+        );
+    }
+}
+
+export default App;
---
+++ Web/react/book_ReactSkill/todolist/src/components/PageTemplate/index.js
@@ -0,0 +1,5 @@
+export {default} from './PageTemplate';
+
+// 컴포넌트 인덱스 파일
+// App 컴포넌트에서 PageTemplate 컴포넌트를 불러올 때, 
+// 경로를 ./PageTemplate/PageTemplate 가 아닌 ./component/PageTemplate 으로 입력하기 위해 만듬
