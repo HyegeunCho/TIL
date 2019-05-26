4bbc21280743dbb6e791d3904974b010bfce3294
Hyegeun Cho <hgcho84.kr@gmail.com>
2019-05-27 00:13:35 +0900

    [Flutter] tutorial-planet 두번째 아티클까지 진행

--- Flutter/tutorial_planet/tutorial_planet/lib/ui/home/HomePage.dart
+++ Flutter/tutorial_planet/tutorial_planet/lib/ui/home/HomePage.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:tutorial_planet/ui/home/GradientAppBar.dart';
+import 'package:tutorial_planet/ui/home/HomePageBody.dart';
 
 class HomePage extends StatelessWidget {
     @override
@@ -8,6 +9,10 @@ class HomePage extends StatelessWidget {
         body: new Column(
             children: <Widget>[
                 new GradientAppBar("treva"),
+                new HomePageBody(),
+                new HomePageBody(),
+                new HomePageBody(),
+                new HomePageBody(),
             ],
         )
     );
