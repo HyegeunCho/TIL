2e6c94d7700dc444f7f77b01aa18011c545a9288
Hyegeun Cho <hgcho84.kr@gmail.com>
2019-08-07 18:38:12 +0900

    프로바이더 패턴 튜토리얼 구현 추가

--- Flutter/tutorial_provider/lib/main.dart
+++ Flutter/tutorial_provider/lib/main.dart
@@ -21,27 +21,75 @@ void main() => runApp(MyApp());
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo', 
-      theme: ThemeData(
-        primarySwatch: Colors.blue, 
-      ), 
-      home: HomePage(),
+    return MultiProvider(
+      providers: [
+        ChangeNotifierProvider(
+          builder: (_) => Counter(),
+        ),
+      ],
+
+      // [MaterialApp] 전역에서 Counter를 사용할 수 있도록 Counsumer를 이용한다.
+      child: Consumer<Counter>(builder: (context, counter, _) {
+        return MaterialApp(
+          title: 'Flutter Demo', 
+          theme: ThemeData(
+            primarySwatch: Colors.blue,
+          ),
+          home: MyHomePage(title: 'Flutter Provider Example'),
+        );
+      }),
     );
   }
 }
 
-class HomePage extends StatelessWidget {
+class MyHomePage extends StatelessWidget {
+  MyHomePage({Key key, this.title}) : super(key:key);
+
+  final String title;
+
+  Widget get _appbar {
+    return AppBar(
+      title: Text(title),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    int _counter = 0;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Provider Demo'), 
-      ), 
+      appBar: _appbar, 
       body: Center(
-        child: Column()
-      )
-    )
+        child: CounterLabel(),
+      ),
+      floatingActionButton: IncrementCounterFab(),
+    );
+  }
+}
+
+/// IncrementCounterFab
+/// 
+/// Fab이 눌리면 [Counter] Provider의 increment를 호출한다.
+class IncrementCounterFab extends StatelessWidget {
+  const IncrementCounterFab({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    final counter = Provider.of<Counter>(context);
+    return FloatingActionButton(
+      onPressed: counter.increment,
+      child: Icon(Icons.add),
+    );
+  }
+}
+
+// CounterLabel
+// build 메서드 안에서 리스너가 변경을 수신하면 count 정보를 변경한다.
+class CounterLabel extends StatelessWidget {
+  const CounterLabel({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context)
+  {
+    final counter = Provider.of<Counter>(context);
+    return Text('${counter.count}');
   }
 }
