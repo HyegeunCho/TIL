23da67fcf065cbfc24f15da1b9ea3d5b5522dedf
Hyegeun Cho <hgcho84.kr@gmail.com>
2021-05-13 19:30:18 +0900

    도메인 주도 개발 철저 입문 예제 프로젝트 추가

---
+++ Books/DDD_basic/List2/FullName.cs
@@ -0,0 +1,14 @@
+namespace List2
+{
+    public class fullName
+    {
+        public string FirstName { get; }
+        public string LastName { get; }
+
+        public fullName(string firstName, string lastName) 
+        {
+            FirstName = firstName;
+            LastName = lastName;
+        }
+    }
+}
---
+++ Books/DDD_basic/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using List2;
+
+namespace DDD_basic
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var valueInstance = new ValueInstance();
+            valueInstance.RunList2();
+        }
+    }
+}
